a44f674955ce9fe297767702ab1b60cd22a83622
Divyang Dantani <divyangdantani786@gmail.com>
2023-05-05 13:33:31 +0530

    - name changed

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         },
       ),
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Divyang\'s Portfolio',
       theme: ThemeData.dark().copyWith(
         primaryColor: primaryColor,
         scaffoldBackgroundColor: bgColor,
